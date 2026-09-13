61e9da53b7d6b03403be08b0923989c85384b511
Patrickbo19 <bpgrimes1997@gmail.com>
2026-09-13 11:15:07 -0400

    Wire wallet routes into machine discovery

--- seller-core.js
+++ seller-core.js
@@ -1,4 +1,5 @@
 require('./income2-personal-market.cjs').install();
 require('./income2-earn-discovery.cjs').install();
 require('./income2-agent-discovery.cjs').install();
+require('./income2-wallet-discovery.cjs').install();
 require('./seller-core-core.js');
