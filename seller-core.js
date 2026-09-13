27dcc56cda709fc20ef9da2cf1c03e506ed0a09f
Patrickbo19 <bpgrimes1997@gmail.com>
2026-09-13 02:34:48 -0400

    Publish agent-network discovery resources

--- seller-core.js
+++ seller-core.js
@@ -1,3 +1,4 @@
 require('./income2-personal-market.cjs').install();
 require('./income2-earn-discovery.cjs').install();
+require('./income2-agent-discovery.cjs').install();
 require('./seller-core-core.js');
